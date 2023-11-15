d0e5d5d380a2a4681ce57f91c1c987643a5a3228
Prakash Balasubramanian <prakash.b08@ad.infosys.com>
2023-11-15 16:05:10 +0530

    Terms and condition page changes

--- App.js
+++ App.js
@@ -1,195 +1,22 @@
-// import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Linking } from 'react-native';
-import Button from '@mui/material/Button';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
-import MenuItem from '@mui/material/MenuItem';
-import TextField from '@mui/material/TextField';
-import Select from '@mui/material/Select';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import {StartPage} from "./Screens/StartPage"
+import StartPage from './Screens/StartPage';
+import TermsService from './Screens/TermsService';
 
-import { CountryPicker, CountryButton } from "react-native-country-codes-picker";
-// import {CountryList} from "react-native-country-codes-picker";
-const image = require('./assets/logoKApp.png');
-function ListHeaderComponent({ countries, lang, onPress }) {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View
-      style={{
-        paddingBottom: 20,
-      }}
-    >
-      <Text>
-        Popular countries
-      </Text>
-      {countries?.map((country, index) => {
-              return (
-                  <CountryButton key={index} item={country} name={country?.name?.[lang || 'en']} onPress={() => onPress(country)} />
-              )
-          })}
-    </View>
-  )
-}
-
-export default function App() {
-  const [show, setShow] = useState(false);
-  const [countryCode, setCountryCode] = useState('+91');
-
-
-  const handleChange = (event) => {
-    setCountryCode(event.target.value);
-  };
-  return (
-    <View style={styles.container}>
-      <Text style={styles.loginHeader}>welcome to kadalaiapp</Text>
-      <View style={styles.logoImg}>
-        <Image source={image} style={{ width: 100, height: 100 }} />
-      </View>
-
-      <View >
-        <Text style={styles.RegNum}> Register Your Number </Text>
-      </View>
-      <View >
-        <Text style={styles.Text1}> Please choose your country code and enter your mobile number to get the verification code.</Text>
-      </View>
-      <View style={styles.TextInput1}>
-        <TouchableOpacity
-          onPress={() => setShow(true)}
-          style={{
-            width: '15%',
-            height: 40,
-            // backgroundColor: 'grey',
-            padding: 10,
-            borderBottomWidth: 1,
-            borderBottomStyle : 'solid',
-            borderBottomColor : '#0000006b',
-          }}
-          
-        >
-          <Text style={{
-            color: 'black',
-            fontSize: 11
-          }}
-          startIcon={<ArrowDropDownIcon />}
-          >
-            {countryCode}
-          </Text>
-        </TouchableOpacity>
-        {/* For showing picker just put show state to show prop */}
-        <CountryPicker
-          show={show}
-          // when picker button press you will get the country object with dial code
-          pickerButtonOnPress={(item) => {
-            setCountryCode(item.dial_code);
-            setShow(false);
-          }}
-          
-          ListHeaderComponent={ListHeaderComponent}
-          popularCountries={['en', 'ua', 'pl']}
-        />
-
-        <TextField
-          id="standard-basic"
-          style={styles.TextInput2}
-          label="Enter Number"
-          variant="standard" />
-
-{/* <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={countryCode}
-          onChange={handleChange}
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select> */}
-
-        <ArrowCircleRightRoundedIcon
-          style={styles.SubmitBtn}
-          color="primary"
-          fontSize='large'>
-        </ArrowCircleRightRoundedIcon>
-      </View>
-
-
-      <View >
-        <Text style={styles.Text2}> Read and accept the KadalaiApp
-          <Text style={{ color: 'blue' }}
-            onPress={() => Linking.openURL('http://google.com')}>
-            Terms of Service and Privacy Policy
-          </Text>
-        </Text>
-      </View>
-
-    </View>
-
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="StartupPage" component={StartPage} />
+        <Stack.Screen name="TermsService" component={TermsService}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    // backgroundColor: 'aqua',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    borderWidth: 2,
-    // borderColor: 'red',
-    fontWeight: 'bold',
-    maxWidth: '390px',
-    paddingRight: '32px',
-    paddingLeft: '32px',
-    marginTop: '120px',
-    marginBottom: '60px',
-    marginRight: 'auto',
-    marginLeft: 'auto'
-  },
-  loginHeader: {
-    marginTop: '70px',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textTransform: 'capitalize',
-  },
-  logoImg: {
-    marginTop: '27px',
-    alignSelf: 'center',
-
-  },
-  RegNum: {
-    marginTop: '30px',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textTransform: 'capitalize',
-
-  },
-  Text1: {
-    marginTop: '30px',
-    textAlign: 'center',
-  },
-  TextInput1: {
-    marginTop: 72,
-    textAlign: 'center'
-  },
-  TextInput2: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    position: 'absolute',
-    left: 75,
-    marginTop: -10,
-  },
-  SubmitBtn: {
-    position: 'absolute',
-    right: 0,
-  },
-  Text2: {
-    marginTop: 81.44,
-    marginBottom: 139,
-    textAlign: 'center',
-  },
-});
+export default App;
---
+++ Screens/TermsService.jsx
@@ -0,0 +1,109 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+
+
+
+export default function TermsService() {
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={{ fontWeight: "bold", fontColor: "black" }}>
+                Terms Of Service<br></br>
+                <br></br>
+            </Text>
+            <Text style={{ fontWeight: "normal", fontColor: "black" }}>
+
+                1. Acceptance of Terms<br></br>
+                By using this kadalai app, you agree to comply with and be bound by the following terms and conditions. If you do not agree to these terms, please do not use the application.<br></br>
+                2. User Conduct<br></br>
+                You agree to use the kadalai app in a responsible and lawful manner.<br></br>
+                You shall not engage in any malicious or disruptive behavior, including spamming, harassment, or distributing harmful content.<br></br>
+                3. Privacy and Data Collection<br></br>
+                We respect your privacy. Please refer to our Privacy Policy for details on how we collect, use, and protect your data.
+                By using this application, you consent to the collection and use of your information as described in our Privacy Policy.<br></br>
+                4. Account Security<br></br>
+                You are responsible for maintaining the confidentiality of your account credentials.<br></br>
+                You agree to notify us immediately of any unauthorized access or use of your account.<br></br>
+                5. User Content<br></br>
+                Any content you share or post on the chat application must not infringe upon the rights of others, including intellectual property rights.<br></br>
+                We do not claim ownership of your content, but you grant us a license to use, modify, or distribute it within the application.<br></br>
+                6. Disclaimer of Warranties<br></br>
+                The kadalai app is provided "as is" without warranties of any kind.
+                We do not guarantee uninterrupted or error-free operation.<br></br>
+                7. Changes to Terms and Conditions<br></br>
+                We reserve the right to modify these terms and conditions at any time. Updated terms will be posted on the application.<br></br>
+                <br></br>
+
+                <Text style={{ fontWeight: "bold", fontColor: "black" }}>
+                    Privacy Policy
+                </Text><br></br><br></br>
+
+                This privacy policy explains how KadalaiApp collects, uses, and shares your personal information when you use our chat application.<br></br>
+
+                Information We Collect<br></br>
+
+                We collect the following types of information from you:<br></br>
+
+                Information you provide to us: This includes information you provide when you create an account, such as your name, email address, and phone number. It may also include information you provide when you chat with other users, such as your profile picture and the content of your chats.
+                Information we collect automatically: This includes information about your device and how you use our chat application, such as your IP address, device type, operating system, and browser type. We may also collect information about your location, but only with your consent.
+                How We Use Your Information<br></br>
+
+                We use your information to provide and improve our chat application. This includes using your information to:<br></br>
+
+                Allow you to create an account and chat with other users.
+                Provide you with support and troubleshoot problems.
+                Send you notifications about new features and updates.
+                Improve our chat application and develop new features.
+                How We Share Your Information<br></br>
+
+                We do not share your personal information with third parties for marketing or advertising purposes. We may share your information with third party service providers who help us provide our chat application, such as web hosting providers and customer support providers. We require all third party service providers to agree to keep your information confidential and to use it only for the purposes of providing us with services.<br></br>
+
+                Your Choices<br></br>
+
+                You have the following choices about how we use your information:<br></br>
+
+                You can opt out of receiving notifications from us by changing your settings in the app.<br></br>
+                You can delete your account and all of your associated data at any time by contacting us.<br></br>
+                Security<br></br>
+
+                We take reasonable steps to protect your information from unauthorized access, use, or disclosure. However, no method of transmission over the internet or electronic storage is 100% secure. Therefore, we cannot guarantee the absolute security of your information.<br></br>
+
+                Changes to This Privacy Policy<br></br>
+
+                We may update this privacy policy from time to time. If we make any significant changes, we will notify you by email or by posting a notice on our website.<br></br>
+
+                In addition to the above, the following privacy policy content may be relevant for a chat application:<br></br>
+
+                Collection and use of location data: If your chat application collects location data, you should explain how you collect this data, how you use it, and how users can opt out of having their location data collected.<br></br>
+                Handling of chat content: You should explain how you handle the content of user chats, such as whether you store or monitor chat content, and whether you disclose chat content to third parties.<br></br>
+                Data retention: You should explain how long you retain user data, and how users can request that their data be deleted.
+                Children's privacy: If your chat application is intended for use by children, you should explain how you comply with applicable children's privacy laws and regulations.
+                You should also tailor your privacy policy to the specific features and functionality of your chat application. For example, if your chat application offers end-to-end encryption, you should explain how this encryption works and how it protects user privacy.<br></br>
+                
+            </Text>
+            <Button onPress={() =>  navigation.navigate('StartPage')} value="Continue">
+                Continue
+            </Button >
+        </ScrollView>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        // backgroundColor: 'aqua',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        borderWidth: 2,
+        // borderColor: 'red',
+        fontWeight: 'bold',
+        maxWidth: '390px',
+        paddingRight: '32px',
+        paddingLeft: '32px',
+        // marginTop: '10px',
+        // marginBottom: '60px',
+        marginRight: 'auto',
+        marginLeft: 'auto'
+    },
+});
